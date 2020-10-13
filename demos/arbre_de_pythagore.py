3d4158a3ea4b96f5604c99c36594c573918a3120
Pierre Feilles <pierre.feilles@gmail.com>
2020-10-13 12:03:13 +0200

    clean arbre_de_pythagore

--- demos/arbre_de_pythagore.py
+++ demos/arbre_de_pythagore.py
@@ -1,8 +1,14 @@
+
 # https://www.mathweb.fr/euclide/2019/03/19/python-turtle-et-un-arbre-de-pythagore/
+# (modifié)
 
 from turtle import *
+import turtle
 import math
 from random import uniform
+
+bob = turtle.Turtle()
+bob.speed(3)
 
 def change_color():
     e1 = uniform(0,1)
@@ -15,30 +21,30 @@ def tree(s,e1,e2,e3):
         return
     square(s,e1,e2,e3)
     e1,e2,e3 = change_color()
-    forward(s)
+    bob.forward(s)
     s1 = s / math.sqrt(2)
-    left(45)
+    bob.left(45)
     tree(s1,e1,e2,e3)
-    right(90)
-    forward(s1)
+    bob.right(90)
+    bob.forward(s1)
     tree(s1,e1,e2,e3)
-    back(s1)
-    left(45)
-    back(s)
+    bob.back(s1)
+    bob.left(45)
+    bob.back(s)
 
 def square(s,e1,e2,e3):
-    pencolor("white")
-    fillcolor(e1,e2,e3)
-    begin_fill()
+    bob.pencolor("white")
+    bob.fillcolor(e1,e2,e3)
+    bob.begin_fill()
     for i in range(4):
-        forward(s)
-        right(90)
-    end_fill()
+        bob.forward(s)
+        bob.right(90)
+    bob.end_fill()
 
 e1,e2,e3 = change_color()
-penup()
-left(180)
-forward(100)
-right(90)
-pendown()
+bob.penup()
+bob.left(180)
+bob.forward(100)
+bob.right(90)
+bob.pendown()
 tree(100,e1,e2,e3)
