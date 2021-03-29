255e27d33f28bcc9e79350764ff48c4fc3666176
Pierre Feilles <pierre.feilles@gmail.com>
2021-03-29 04:34:34 +0200

    update content

--- antigua/antigua-20210329-02-circles-semicircles-ex.py
+++ antigua/antigua-20210329-02-circles-semicircles-ex.py
@@ -4,10 +4,6 @@
 # https://github.com/pfei/tutorials/blob/main/images/antigua-20210329-03-circles-semicircles-a-solution.png
 
 
-
-# https://docs.python.org/3/library/turtle.html
-
-# adapted from :
 # https://docs.python.org/3/library/turtle.html#turtle.circle
 
 import turtle
